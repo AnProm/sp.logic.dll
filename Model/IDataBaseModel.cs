4fc305b895b99ecc979f8bb64b6184c0f0e4fbd1
Kuurcha <yhyr@mail.ru>
2021-04-28 10:53:14 +0300

    SP-16: Interface Changed

---
+++ Model/IDataBaseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+namespace Logic.Model
+{
+    public interface IDataBaseModel
+    {
+        string mode { get; set; }
+
+        string path { get; set; }
+        ICollection<DataBaseObject> Delete(DataBaseObject objectToDelete);
+        ICollection<DataBaseObject> Add(DataBaseObject objectToAdd);
+        ICollection<DataBaseObject> Update(DataBaseObject objectToUpdate);
+        ICollection<DataBaseObject> Load();
+        void Save(string pathToSave);
+       
+    }
+}
